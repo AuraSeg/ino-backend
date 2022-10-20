b21851b70b2a6d0af233d8c02697351c25e5a429
AuraSeg <ada_hack131@hackademy.mx>
2022-10-20 16:48:59 -0500

    Updating dependencies

--- ino/ino/settings.py
+++ ino/ino/settings.py
@@ -92,13 +92,28 @@ WSGI_APPLICATION = 'ino.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+PG_DB = config('POSTGRES_DB', default='')
+PG_USER = config('POSTGRES_USER', default='')
+PG_PASSWD = config('POSTGRES_PASSWORD', default='')
+PG_HOST = config('POSTGRES_HOST', default='127.0.0.1')
+PG_PORT = config('POSTGRES_PORT', cast=int, default=5432)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': PG_DB,
+        'USER': PG_USER,
+        'PASSWORD': PG_PASSWD,
+        'HOST': PG_HOST,
+        'PORT': PG_PORT,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
